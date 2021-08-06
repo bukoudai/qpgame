54a26c90b3abfcf6d97659b0c89fb33537fb0572
bukoudai <secret0me@qq.com>
2021-08-06 17:57:30 +0800

    去除修改名片及其他功能 以防被封

--- src/main/java/com/bukoudai/qpgame/configuration/QQDefautBotConfiguration.java
+++ src/main/java/com/bukoudai/qpgame/configuration/QQDefautBotConfiguration.java
@@ -2,18 +2,13 @@ package com.bukoudai.qpgame.configuration;
 
 import com.bukoudai.qpgame.msgservice.FriendMessageConsumer;
 import com.bukoudai.qpgame.msgservice.GroupMessageEventService;
-import com.bukoudai.qpgame.msgservice.GroupTalkativeChangeEventConsumer;
-import com.bukoudai.qpgame.msgservice.MemberCardChangeEventConsumer;
 import com.bukoudai.qpgame.service.BotsService;
-import com.bukoudai.qpgame.service.UserService;
 import com.bukoudai.qpgame.utlis.BotUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.event.events.FriendMessageEvent;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
-import net.mamoe.mirai.event.events.GroupTalkativeChangeEvent;
-import net.mamoe.mirai.event.events.MemberCardChangeEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,7 +19,6 @@ public class QQDefautBotConfiguration {
 
     private final GroupMessageEventService groupMessageEventService;
     private final BotsService botsService;
-    private final UserService userService;
 
     @Bean("myBot")
     public Bot setDefaultBot() {
@@ -33,8 +27,6 @@ public class QQDefautBotConfiguration {
         if (bot != null) {
             long botId = bot.getId();
             bot.getEventChannel().subscribeAlways(FriendMessageEvent.class, new FriendMessageConsumer());
-            bot.getEventChannel().subscribeAlways(MemberCardChangeEvent.class, new MemberCardChangeEventConsumer(botId,userService));
-            bot.getEventChannel().subscribeAlways(GroupTalkativeChangeEvent.class, new GroupTalkativeChangeEventConsumer());
             bot.getEventChannel().subscribeAlways(GroupMessageEvent.class, (event) ->
                     BotUtils.sendMsg(event.getGroup(), event, groupMessageEventService.executCommand(event, botId))
             );
