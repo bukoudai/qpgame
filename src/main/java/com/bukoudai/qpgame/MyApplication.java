2b997837554930990c13a1a25f671a0bb1bc0648
bukoudai <secret0me@qq.com>
2021-11-16 10:30:50 +0800

    增加输出PID功能

--- src/main/java/com/bukoudai/qpgame/MyApplication.java
+++ src/main/java/com/bukoudai/qpgame/MyApplication.java
@@ -1,5 +1,6 @@
 package com.bukoudai.qpgame;
 
+import cn.hutool.core.io.FileUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
 
 /**
  * @author bukoudai
@@ -21,7 +26,30 @@ public class MyApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MyApplication.class, args);
-    log.info("项目启动");
+    String name = ManagementFactory.getRuntimeMXBean().getName();
+
+    log.info(name);
+
+// get pid
+
+    String pid = name.split("@")[0];
+
+    log.info("项目启动 Pid is:" + pid);
+    try {
+      File pidFile = new File("pid");
+      if (!pidFile.exists()) {
+        pidFile.createNewFile();
+      }
+      FileUtil.writeUtf8String(pid, pidFile);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      File pidFile = new File("pid");
+      if (pidFile.exists()) {
+        pidFile.delete();
+      }
+    }));
   }
 
 }
